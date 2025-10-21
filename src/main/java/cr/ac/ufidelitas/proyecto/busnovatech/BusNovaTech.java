73a8a322ce90bdbea37a2d58398dd7023409227b
jiro <jiroaku@proton.me>
2025-10-20 22:23:50 -0600

    first commit

---
+++ src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.ufidelitas.proyecto.busnovatech;
+
+/**
+ *
+ * @author jiroaku
+ */
+public class BusNovaTech {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
