80622439afdfd84327430820f361f8199a14e14b
Samuel <samimenagarro@gmail.com>
2025-10-20 22:49:30 -0600

    Prueba

--- src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
+++ src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
@@ -12,5 +12,6 @@ public class BusNovaTech {
 
     public static void main(String[] args) {
         System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Samuel");
     }
 }
