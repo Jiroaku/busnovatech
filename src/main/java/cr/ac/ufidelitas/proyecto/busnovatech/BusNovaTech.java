6d5cf6f8ff7af2303a667e4f4bf682743ffc3f0d
Gerald-HF <geraldobed05@gmail.com>
2025-10-20 23:01:05 -0600

    test commit

--- src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
+++ src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
@@ -12,5 +12,14 @@ public class BusNovaTech {
 
     public static void main(String[] args) {
         System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
+        System.out.println("Hello World!, Andrew");
     }
 }
