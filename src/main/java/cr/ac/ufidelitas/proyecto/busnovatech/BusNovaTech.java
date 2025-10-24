3ba7216f70ad030de8aee98a72347aa0f23d0a2a
Gerald-HF <geraldobed05@gmail.com>
2025-10-24 17:52:03 -0600

    Update BusNovaTech.java

--- src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
+++ src/main/java/cr/ac/ufidelitas/proyecto/busnovatech/BusNovaTech.java
@@ -5,6 +5,8 @@ package cr.ac.ufidelitas.proyecto.busnovatech;
  *
  * @author jiroaku
  */
+
+//PUSE LA VERSION 2.8.9 DE GSON COMO EL PROFE
 public class BusNovaTech {
 
     public static void main(String[] args) {
